044c2f5c46e07554e1b8c4055d0a1383994a3b3d
yvonnevoxy <daudiyvonne@gmail.com>
2018-05-19 13:11:53 +0300

    print today is on saturday

---
+++ index.py
@@ -0,0 +1,4 @@
+print('Today is on saturday')
+print('This is the 1st class of Andela')
+print('Its alot of fun')
+print('We learned and interacted alot,its tricky but it only needs a focussed mind of a  person who is ready to learn')
